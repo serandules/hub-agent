b8dd7742fec4f51729fcb14a04266339367c9f83
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-13 01:08:21 +0530

    updating

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var proxy = require('proxy');
+
 module.exports = function (server) {
     server.listen(function () {
         var address = server.address();
@@ -11,4 +13,41 @@ module.exports = function (server) {
             pid: process.pid
         });
     });
+};
+
+module.exports.proxy = function (self) {
+    var options = {
+        'accounts.serandives.com': 4004,
+        'auto.serandives.com': 4004,
+        'localhost': 4004
+    };
+    var allow = function (drone) {
+        var domain = drone.domain;
+        if (domain.indexOf('*.') === -1) {
+            domain = domain.substring(2);
+            if (domain === self) {
+                return;
+            }
+        } else if (domain !== self) {
+            return;
+        }
+        var opts = options[domain] || (options[domain] = []);
+        opts.drones.push({
+            ip: drone.ip,
+            port: drone.port
+        });
+    };
+    var prxy;
+    process.on('message', function (data) {
+        switch (data.event) {
+            case 'drone joined':
+                prxy = proxy(allow);
+                break;
+            case 'drone left':
+                break;
+        }
+    });
+    return function (req, res, next) {
+        prxy ? prxy(req, res, next) : next();
+    };
 };
