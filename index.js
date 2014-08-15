d9fb7745645906245588b2554cadfb441c898569
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-16 00:08:17 +0530

    updating

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function (server) {
             return;
         }
         process.send({
-            event: 'started',
+            event: 'drone started',
             address: address,
             pid: process.pid
         });
@@ -16,11 +16,7 @@ module.exports = function (server) {
 };
 
 module.exports.proxy = function (self) {
-    var options = {
-        'accounts.serandives.com': 4004,
-        'auto.serandives.com': 4004,
-        'localhost': 4004
-    };
+    var options = {};
     var allow = function (drone) {
         var domain = drone.domain;
         if (domain.indexOf('*.') === -1) {
@@ -37,17 +33,42 @@ module.exports.proxy = function (self) {
             port: drone.port
         });
     };
+    var disallow = function (drone) {
+        var opts = options[drone.domain];
+        if (!opts) {
+            return;
+        }
+        var i, length = opts.length;
+        for (i = 0; i < length; i++) {
+            var opt = opts[i];
+            if (opt.ip === drone.ip && opt.port === drone.port) {
+                opt.splice(i, 1);
+                break;
+            }
+        }
+    };
     var prxy;
     process.on('message', function (data) {
         switch (data.event) {
             case 'drone joined':
-                prxy = proxy(allow);
+                console.log('drone jointed : ' + JSON.stringify(data.drone));
+                allow(data.drone);
                 break;
             case 'drone left':
+                console.log('drone left : ' + JSON.stringify(data.drone));
+                disallow(data.drone);
+                break;
+            case 'drones list':
+                console.log('drones : ' + JSON.stringify(data.drones));
+                data.drones.forEach(function (drone) {
+                    allow(drone);
+                });
                 break;
         }
+        prxy = proxy(options);
     });
     return function (req, res, next) {
+        console.log('proxying requests with : ' + JSON.stringify(options));
         prxy ? prxy(req, res, next) : next();
     };
 };
