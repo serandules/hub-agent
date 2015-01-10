fa5252de910bebf05c35adca0872d506a85986e5
ruchiraw <ruchira.wageesha@gmail.com>
2015-01-10 20:26:00 +0530

    updating

--- index.js
+++ index.js
@@ -1,32 +1,91 @@
 var debug = require('debug')('serandules:hub-agent');
+var cluster = require('cluster');
+var http = require('http');
 var proxy = require('proxy');
 var uuid = require('node-uuid');
 
+var cpus = require('os').cpus().length;
+
 var configs = {};
 
-module.exports = function (server, port) {
-    var serve = function () {
-        var address = server.address();
-        debug('listening on ' + JSON.stringify(address));
+var workers = [];
+
+var queue = {};
+
+module.exports = function (run) {
+    if (cluster.isWorker) {
+        run();
+        return;
+    }
+    // Fork workers.
+    var worker;
+    for (var i = 0; i < cpus; i++) {
+        worker = cluster.fork();
+        worker.on('message', (function (worker) {
+            return function (data) {
+                /*if (data.event !== 'drone configed') {
+                 return;
+                 }
+                 debug('event:drone configed id:' + data.id);
+                 var fn = configs[data.id];
+                 fn(data.value);
+                 delete configs[data.id];*/
+                debug('++++++++++++++++++++++++++++' + data.id + ' ' + data.event);
+                queue[data.id] = worker;
+                process.send(data);
+            };
+        }(worker)));
+        workers.push(worker);
+    }
+    cluster.on('exit', function (worker, code, signal) {
+        debug('worker ' + worker.process.pid + ' died');
+    });
+    cluster.on('listening', function (worker, address) {
+        if (--cpus > 0) {
+            debug('worker started listening');
+            return;
+        }
         if (!process.send) {
             return;
         }
+        var broadcast = function (data) {
+            workers.forEach(function (worker) {
+                worker.send(data);
+            });
+        };
+        process.on('message', function (data) {
+            debug('++++++++++++++++++++++++++++--------------------============================' + data.id + ' ' + data.event);
+            var id;
+            switch (data.event) {
+                case 'drone configed':
+                    id = data.id;
+                    debug('event:drone configed id:' + id);
+                    var fn = configs[id];
+                    fn(data.value);
+                    delete configs[id];
+                    break;
+                case 'drone init':
+                    broadcast(data);
+                    break;
+                case 'drone joined':
+                    broadcast(data);
+                    break;
+                case 'drone left':
+                    broadcast(data);
+                    break;
+                case 'self domain':
+                    broadcast(data);
+                    break;
+                default:
+            }
+        });
         process.send({
             event: 'drone started',
             address: address,
             pid: process.pid
         });
-        process.on('message', function (data) {
-            if (data.event !== 'drone configed') {
-                return;
-            }
-            debug('event:drone configed id:' + data.id);
-            var fn = configs[data.id];
-            fn(data.value);
-            delete configs[data.id];
-        });
-    };
-    port ? server.listen(port, serve) : server.listen(serve);
+        debug('drone is advertised ' + address.address + ':' + address.port);
+    });
 };
 
 module.exports.proxy = function () {
@@ -103,7 +162,10 @@ module.exports.proxy = function () {
         return allowed;
     };
 
-    var prxy;
+    var prxy = function (req, res, next) {
+        debug('bypassing proxying');
+        next();
+    };
     var initialized = false;
     var pending = [];
 
@@ -124,6 +186,7 @@ module.exports.proxy = function () {
     };
 
     process.on('message', function (data) {
+        debug('-------------------------------------------------------' + data.event);
         debug('message:' + data.event);
         debug(data);
         switch (data.event) {
