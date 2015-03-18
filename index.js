b585c48ff4a878017396c4fee40588e62c362a36
ruchiraw <ruchira.wageesha@gmail.com>
2015-03-19 03:11:43 +0530

    updating

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var log = require('logger')('hub-agent');
 var fs = require('fs');
+var uuid = require('node-uuid');
 var https = require('https');
 var io = require('socket.io-client');
 
 var configs = require('hub-configs');
 
-module.exports = function (ns, done) {
+var agent = function (ns, done) {
     var agent = new https.Agent({
         key: fs.readFileSync(configs.ssl.key),
         cert: fs.readFileSync(configs.ssl.cert),
@@ -24,3 +25,31 @@ module.exports = function (ns, done) {
 
     done(false, socket);
 };
+
+var config;
+
+var queue = [];
+
+var conf = function (name, done) {
+    var id = uuid.v4();
+    config.emit(config, id, name);
+    config.once(id, function (value) {
+        done(value);
+    });
+};
+
+agent('/configs', function (err, io) {
+    io.once('connection', function () {
+        config = io;
+        queue.forEach(function (done) {
+            done(conf);
+        });
+    });
+});
+
+module.exports = agent;
+
+module.exports.config = function (done) {
+    config ? done(conf) : queue.push(done);
+};
+
